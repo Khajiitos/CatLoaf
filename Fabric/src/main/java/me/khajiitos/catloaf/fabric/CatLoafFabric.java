e2b2205f61ba8ee0e2e6cd76d5796f56bac37958
khajiitos <khajiit112@gmail.com>
2024-02-26 16:19:33 +0100

    Fix fabric

--- Fabric/src/main/java/me/khajiitos/catloaf/fabric/CatLoafFabric.java
+++ Fabric/src/main/java/me/khajiitos/catloaf/fabric/CatLoafFabric.java
@@ -2,7 +2,7 @@ package me.khajiitos.catloaf.fabric;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class WorldPlayTimeFabric implements ClientModInitializer {
+public class CatLoafFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
