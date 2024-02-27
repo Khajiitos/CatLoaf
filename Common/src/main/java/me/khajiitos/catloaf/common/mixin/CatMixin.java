44fe697175999cf536d0bbbe9c985114c6212b9a
pt20krezolekk <103023548+pt20krezolekk@users.noreply.github.com>
2024-02-27 09:13:17 +0100

    100% loaf chance with bread, NeoForge MATCH_VERSION

--- Common/src/main/java/me/khajiitos/catloaf/common/mixin/CatMixin.java
+++ Common/src/main/java/me/khajiitos/catloaf/common/mixin/CatMixin.java
@@ -5,20 +5,29 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.animal.Cat;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Cat.class)
 public abstract class CatMixin extends TamableAnimal implements ILoafable {
     @Unique
-    private static final EntityDataAccessor<Boolean> LOAFING = SynchedEntityData.defineId(Cat.class, EntityDataSerializers.BOOLEAN);;
+    private boolean catloaf$forceLoaf = false;
+
+    @Unique
+    @SuppressWarnings("all")
+    private static final EntityDataAccessor<Boolean> LOAFING = SynchedEntityData.defineId(Cat.class, EntityDataSerializers.BOOLEAN);
 
     protected CatMixin(EntityType<? extends TamableAnimal> $$0, Level $$1) {
         super($$0, $$1);
@@ -42,7 +51,8 @@ public abstract class CatMixin extends TamableAnimal implements ILoafable {
     @Override
     public void setInSittingPose(boolean sitting) {
         super.setInSittingPose(sitting);
-        this.setLoafing(sitting && this.random.nextBoolean());
+        this.setLoafing(sitting && (catloaf$forceLoaf || this.random.nextBoolean()));
+        catloaf$forceLoaf = false;
     }
 
     @Inject(at = @At("TAIL"), method = "addAdditionalSaveData")
@@ -55,5 +65,12 @@ public abstract class CatMixin extends TamableAnimal implements ILoafable {
     @Inject(at = @At("TAIL"), method = "readAdditionalSaveData")
     public void readAdditionalSaveData(CompoundTag compoundTag, CallbackInfo ci) {
         this.setLoafing(compoundTag.getBoolean("IsLoafing"));
+    }
+
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/animal/Cat;setOrderedToSit(Z)V", ordinal = 0, shift = At.Shift.AFTER), method = "mobInteract")
+    public void mobInteract(Player player, InteractionHand interactionHand, CallbackInfoReturnable<InteractionResult> callbackInfo) {
+        if (player.getItemInHand(interactionHand).is(Items.BREAD) && this.isOrderedToSit()) {
+            catloaf$forceLoaf = true;
+        }
     }
 }
