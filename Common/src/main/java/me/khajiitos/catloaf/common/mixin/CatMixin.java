6a34ccbfa6ac6e66553a9399f58178bc9110f2fe
dima_dencep <dima_dencep@redlance.org>
2024-05-17 14:58:33 +0700

    Update to 1.20.6

--- Common/src/main/java/me/khajiitos/catloaf/common/mixin/CatMixin.java
+++ Common/src/main/java/me/khajiitos/catloaf/common/mixin/CatMixin.java
@@ -35,8 +35,8 @@ public abstract class CatMixin extends TamableAnimal implements ILoafable {
     }
 
     @Inject(at = @At("TAIL"), method = "defineSynchedData")
-    public void defineSynchedData(CallbackInfo ci) {
-        this.entityData.define(LOAFING, false);
+    public void defineSynchedData(SynchedEntityData.Builder $$0, CallbackInfo ci) {
+        $$0.define(LOAFING, false);
     }
 
     @Override
--- NeoForged/src/main/java/me/khajiitos/catloaf/neoforged/CatLoafNeoForged.java
+++ NeoForged/src/main/java/me/khajiitos/catloaf/neoforged/CatLoafNeoForged.java
@@ -5,18 +5,18 @@ import me.khajiitos.catloaf.common.config.CatLoafConfig;
 import me.khajiitos.catloaf.common.config.cloth.ClothConfigCheck;
 import me.khajiitos.catloaf.common.config.cloth.ClothConfigScreenMaker;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLLoader;
-import net.neoforged.neoforge.client.ConfigScreenHandler;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(CatLoaf.MOD_ID)
 public class CatLoafNeoForged {
-    public CatLoafNeoForged() {
+    public CatLoafNeoForged(ModContainer container) {
         CatLoafConfig.init();
 
         if (FMLLoader.getDist() == Dist.CLIENT && ClothConfigCheck.isInstalled()) {
-            ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory(ClothConfigScreenMaker::create));
+            container.registerExtensionPoint(IConfigScreenFactory.class, ClothConfigScreenMaker::create);
         }
     }
 }
