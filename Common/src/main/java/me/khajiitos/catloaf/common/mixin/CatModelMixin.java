5c0af7c258bc546cdc2ea64481cb6ca7b0d907fb
khajiitos <khajiit112@gmail.com>
2024-12-02 07:51:53 +0100

    Hide tail when loafing feature, 1.1.3

--- Common/src/main/java/me/khajiitos/catloaf/common/mixin/CatModelMixin.java
+++ Common/src/main/java/me/khajiitos/catloaf/common/mixin/CatModelMixin.java
@@ -20,6 +20,12 @@ public abstract class CatModelMixin extends FelineModel<CatRenderState> {
     public void setupAnim(@NotNull CatRenderState catRenderState) {
         super.setupAnim(catRenderState);
 
+        // May be overriden by the loafify function
+        // so we are restoring it every setupAnim call, because it isn't restored by the game
+        // hopefully this doesn't clash with functionality of other mods
+        tail1.visible = true;
+        tail2.visible = true;
+
         if (catRenderState instanceof ILoafable loafable && catRenderState.isSitting && loafable.isLoafing()) {
             // Exported into a separate function for easier debugging,
             // since "Compile And Reload File" doesn't work on Mixins
